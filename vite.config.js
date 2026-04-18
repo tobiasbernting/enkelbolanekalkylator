abbcf6ec0b52461c932b91481305eab6d6e7f533
Tobias Bernting <utb@Tobiass-MacBook-Air.local>
2026-04-18 16:57:26 +0200

    deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/enkelbolanekalkylator/',
     plugins: [react()],
     server: {
         port: 5173,
